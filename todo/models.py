2b345c14abe20afb19bf5a0918175f0c49ff6b16
Shivam Rajvir <shivamrajvir@gmail.com>
2020-11-04 00:23:36 +0530

    migration changes for todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(blank=True, max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
